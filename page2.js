a90b3b87dab1bc9bd800ffd82f30f7f4dcd1437d
eja37 <eja37@njit.edu>
2024-11-21 10:58:09 -0500

    update
    made question 1 display correctly this time on console

--- page2.js
+++ page2.js
@@ -5,6 +5,16 @@ let pagename = "Flashcards";
 console.log(`Hello! Welcome, ${username} to Study Sphere's ${pagename}!`);
 
 //Flashcards, these will have the splice tool //
-let question = ["Let this be question 1", "Let this be question 2", "Let this be question 3"]
-Let answers = ["answer to question 1", "answer to question 2", "answer to question 3"];
+let questions = ["Let this be question 1", "Let this be question 2", "Let this be question 3"]
+let answers = ["answer to question 1", "answer to question 2", "answer to question 3"];
+
+//this could track the flashcard//
+let currentIndex = 0;
+let showQuestion = true; //This is a boolean//
+
+let display = "Question: " + questions[currentIndex];
+
+console.log(display);
+
+
 
