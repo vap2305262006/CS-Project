bc24a15f9f68b8109250f4ac966e3b5b2eaf3d05
eja37 <eja37@njit.edu>
2024-11-21 10:06:39 -0500

    Update to page 2
    added question 1 to show on console

--- page2.js
+++ page2.js
@@ -4,7 +4,7 @@ let pagename = "Flashcards";
 console.log(`Hello! Welcome, ${username} to Study Sphere's ${pagename}!`);
 
 //Flashcard//
-let flashcard = [
+let = flashcard = [
     "Let this be question 1 / Let this be answer to question 1" ,
     "Let this be question 2 / Let this be answer to question 2" ,
     "Let this be question 3 / Let this be answer to question 3"
@@ -15,3 +15,7 @@ let index = 0;
 let showQuestion = 1;
 
 //We should display one question at a time if we can in JS//
+let currentFlashcard = flashcard[index];
+let splitFlashcard = currentFlashcard.split("/");
+let display = showQuestion ? `Question: ${splitFlashcard[0]}`: `Answer: ${splitFlashcard[1]}`;
+console.log(display);
