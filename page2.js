a6f341c6f7f5360b73995e95b2659217444975a6
eja37 <eja37@njit.edu>
2024-11-21 10:55:11 -0500

    questions for flashcards

--- page2.js
+++ page2.js
@@ -5,3 +5,6 @@ let pagename = "Flashcards";
 console.log(`Hello! Welcome, ${username} to Study Sphere's ${pagename}!`);
 
 //Flashcards, these will have the splice tool //
+let question = ["Let this be question 1", "Let this be question 2", "Let this be question 3"]
+Let answers = ["answer to question 1", "answer to question 2", "answer to question 3"];
+
