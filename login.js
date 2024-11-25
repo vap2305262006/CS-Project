6f0fa553cd75556d4ddb8e98581c5ea45bd54510
vap2305262006 <vap23@njit.edu>
2024-11-25 10:15:11 -0500

    login js update

--- login.js
+++ login.js
@@ -1,20 +1,39 @@
-var people = [
-    {
-        username: VeronicaTheBeast,
-        password: vp123
 
-    },
-    {
-        username: EdnaTheGreat,
-        password: ed246
-    },
-    {
-        username: Sara45,
-        password: sl897
+let username = "VeronicaTheBeast"
+let passowrd = "vap123"
 
-    },
-    {
-        username: Jamiestar,
-        password: jam987
-    }
-]
+function checkValues (){
+    if(document.getElementsByClassName('uname').value==username){
+    console.log('Login Success')
+}
+else{
+    checkValue.preventdefault();
+}
+
+if(document.getElementsByClassName('pname').value==password){
+    console.log('Login Success')
+}
+else{
+    checkValue.preventdefault();
+}
+}
+
+let username2 = "EdnaTheGreat"
+let passowrd2 = "ej456"
+
+function checkValues (){
+    if(document.getElementsByClassName('uname').value==username2){
+    console.log('Login Success')
+}
+else{
+    checkValue.preventdefault();
+}
+
+if(document.getElementsByClassName('pname').value==password2){
+    console.log('Login Success')
+}
+else{
+    checkValue.preventdefault();
+}
+}
+
