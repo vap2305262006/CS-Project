350c5442e3a67591cc0d11919a35587ab06b6f3a
eja37 <eja37@njit.edu>
2024-12-07 04:59:56 -0500

    images

--- flashcard.js
+++ flashcard.js
@@ -1,9 +1,29 @@
 function answerTo1(){
-    document.getElementById('answer1').innerText = "2"
+    document.getElementById('answer1').innerText = "The answer is 2"
 }
 document.getElementById('Q1').addEventListener('click', answerTo1)
 
 function answerTo2(){
-    document.getElementById('answer1').innerText = "6"
+    document.getElementById('answer2').innerText = "The answer is 4"
 }
 document.getElementById('Q2').addEventListener('click', answerTo2)
+
+function answerTo3(){
+    document.getElementById('answer3').innerText = "The answer is 20"
+}
+document.getElementById('Q3').addEventListener('click', answerTo3)
+
+function answerTo4(){
+    document.getElementById('answer4').innerText = "The answer is 27"
+}
+document.getElementById('Q4').addEventListener('click', answerTo4)
+
+function answerTo5(){
+    document.getElementById('answer5').innerText = "The answer is 45"
+}
+document.getElementById('Q5').addEventListener('click', answerTo5)
+
+function answerTo6(){
+    document.getElementById('answer6').innerText = "The answer is 8"
+}
+document.getElementById('Q6').addEventListener('click', answerTo6)
