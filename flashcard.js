1a1233b6c15ab71a82834ac917a104e8cb2bdedb
eja37 <eja37@njit.edu>
2024-12-07 04:04:22 -0500

    update

---
+++ flashcard.js
@@ -0,0 +1,9 @@
+function answerTo1(){
+    document.getElementById('answer1').innerText = "2"
+}
+document.getElementById('Q1').addEventListener('click', answerTo1)
+
+function answerTo2(){
+    document.getElementById('answer1').innerText = "6"
+}
+document.getElementById('Q2').addEventListener('click', answerTo2)
